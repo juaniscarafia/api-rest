2a8303e1a3ca6de392df922411cd82838acec881
Juan Ignacio Scarafía <juani.scarafia@gmail.com>
2020-04-20 10:45:41 -0300

    Encrypt password

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,11 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = app => {
     const Users = app.db.models.Users;
+    const bcrypt = require('bcryptjs');
+    const saltRounds = 10;
+    const myPlaintextPassword = 's0/\/\P4$$w0rD';
+    const someOtherPlaintextPassword = 'not_bacon';
 
     app.route('/users/')
         .get((req,res) => {
@@ -12,7 +18,14 @@ module.exports = app => {
                 });
         })
         .post((req, res) => {
-            Users.create(req.body)
+            const usr = req.body;
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hash = bcrypt.hashSync(myPlaintextPassword, salt);
+
+            usr.password = hash;
+            req.body.password = hash;
+            
+            Users.create(usr)
                 .then(result => res.json(result))
                 .catch(error =>{
                     res.status(412).json({msg: error.message});
